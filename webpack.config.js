7c25f544f48f1de3bca343cd1df416d1fdb9ca37
Abhilash D K <abhilashdk2014@outlook.com>
2017-12-08 02:49:07 +0530

    Update webpack.config.js
    Support for svg, font anf image files added

--- webpack.config.js
+++ webpack.config.js
@@ -17,22 +17,26 @@ module.exports = {
             // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
             { test: /\.tsx?$/, loader: "ts-loader" },
             {
-                test: /\.css/,
-                use: [
-                    { loader: "style-loader" },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules:true
-                        }
-                    }
-                ] 
-            }
+                test: /\.css$/, 
+                use: [ 
+                        { loader: 'style-loader' }, 
+                        { 
+                            loader: 'css-loader', 
+                            options: { 
+                                modules: true,
+                                localIdentName: ('[name]__[local]__[hash:base64:5]')
+     
+                            } 
+                        } 
+                    ] 
+            },
+            { test: /\.(png|jpg|gif)$/, use: 'url-loader?limit=15000&name=images/[name].[ext]' },
+            { test: /\.eot(\?v=\d+.\d+.\d+)?$/, use: 'file-loader?&name=fonts/[name].[ext]' },
+            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff&name=fonts/[name].[ext]' },
+            { test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/octet-stream&name=fonts/[name].[ext]' },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=image/svg+xml&name=images/[name].[ext]' }, 
         ],
-
-        /*preLoaders: [
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { test: /\.js$/, loader: "source-map-loader" }
-        ]*/
     }
 }
+
+
